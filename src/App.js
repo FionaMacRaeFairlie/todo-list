e7cc2d25cc722f855dc6c9f923e5078962738849
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2021-10-29 19:18:31 +0100

    to do list with hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
-
+import datasource from "./data.js";
+import { useEffect, useState, useCallback } from "react";
+import query from "./Query";
+import Header from "./header";
+import ToDoList from "./ToDoList";
+import ToDoForm from './ToDoForm';
+ 
 function App() {
-  return (
+  const [ toDoList, setToDoList ] = useState([{
+    "todo": " ",
+    "done": " "
+  }]);
+
+  const fetchData = useCallback(() => {
+    const queryText = JSON.stringify(query);
+      fetch(datasource.baseURL, {
+       method: "POST",
+       headers: datasource.headers,
+       body: queryText,
+     }).then((response) => response.json())
+       .then((data) => {
+         const datalist = data.data.list;
+          setToDoList(datalist);
+       })
+       .catch((err) => {
+         console.log(err);
+       });
+   }, []);
+ 
+   useEffect(() => {
+     fetchData();
+   }, [fetchData]);
+
+   function handleClear() {
+    const clearQuery = {
+     query: `
+     mutation{clearAll {
+      todo
+      done
+    }}
+       `,
+ };
+    const queryText = JSON.stringify(clearQuery);
+    let cleared = [{"todo": "", "done": ""}];
+     fetch(datasource.baseURL, {
+       method: "POST",
+       headers: datasource.headers,
+       body:queryText,
+     }).then((response) => response.json())
+     .then(setToDoList(cleared))
+       .catch((err) => {
+         console.log(err);
+       });
+   };
+
+   const addTask = (userInput ) => {
+     const dataQuery = {
+     query: `
+     mutation {addtodo(
+         input: {todo:"${userInput}", 
+           done:"false" }
+       ){todo,done}}
+       `,
+ };
+    const queryText = JSON.stringify(dataQuery);
+     fetch(datasource.baseURL, {
+       method: "POST",
+       headers: datasource.headers,
+       body:queryText,
+     }).then((response) => response.json())
+     .then(fetchData)
+       .catch((err) => {
+         console.log(err);
+       });
+   };
+
+ const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+    let taskProgress="";
+    if(task.todo === String(id)){
+      if(task.done==="true"){ taskProgress="false" }
+       else{
+         taskProgress="true" }
+     }
+    return task.todo === String(id) ? { ...task, done: taskProgress} : { ...task};
+  });
+  setToDoList(mapped);
+  }
+
+  const clearCompleted = () => {
+    handleClear();
+    let incomplete = toDoList.filter(todo => {
+      return todo.done ==="false";
+    });
+    incomplete.forEach(element => {
+      addTask(element.todo);
+    });
+  }
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ToDoForm addTask={addTask}/>
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle}  handleClear={handleClear} clearCompleted={clearCompleted}/>
+      
     </div>
   );
 }
 
+ 
 export default App;
